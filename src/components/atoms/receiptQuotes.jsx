19ab9821697be87807ea2f02ed58dbd025678176
jayjeong8 <zuni4242@gmail.com>
2022-08-28 03:21:14 +0900

    refactor : getQuotes 주석 삭제

--- src/components/atoms/receiptQuotes.jsx
+++ src/components/atoms/receiptQuotes.jsx
@@ -2,19 +2,6 @@ import styled from "styled-components";
 import { useEffect, useState } from "react";
 
 export function ReceiptQuotes() {
-  // const getQuotes = async () => {
-  //   try{
-  //     const res = await fetch("https://api.adviceslip.com/advice");
-  //
-  //     if(!res.ok) throw new Error("오늘은 명언이 없어도 괜찮은 하루일지도 몰라요.");
-  //
-  //     const data = await res.json();
-  //     setQuotes(data);
-  //   }catch(e){
-  //     console.error(e);
-  //   }
-  // }
-
   const [quotesState, setQuotes] = useState();
 
   useEffect(() => {
