806217bfcc2b39e0f4bf0ad492f720a26f6d1618
Madang <chooi9522@gmail.com>
2022-08-27 21:18:19 +0900

    feat : 더미데이터 생성, getTodoList API 작성

---
+++ src/controllers/dummy.js
@@ -0,0 +1,103 @@
+// TODO: date양식 맞추기
+export const dummyTodosList = [
+  // 8월 27일 토요일
+  {
+    task: "풀리퀘 확인하기",
+    date: "2022-08-27T12:41:21",
+    isDone: true,
+    timer: 30,
+  },
+  {
+    task: "Homework",
+    date: "2022-08-27T12:41:21",
+    isDone: true,
+    timer: 25,
+  },
+  {
+    task: "Yoga Class",
+    date: "2022-08-27T12:41:21",
+    isDone: false,
+    timer: 180,
+  },
+  // 8월 28일 일요일
+  {
+    task: "풀리퀘 확인하기",
+    date: "2022-08-28T12:41:21",
+    isDone: true,
+    timer: 30,
+  },
+  {
+    task: "과제",
+    date: "2022-08-28T12:41:21",
+    isDone: false,
+    timer: 25,
+  },
+  {
+    task: "Yoga Class",
+    date: "2022-08-28T12:41:21",
+    isDone: true,
+    timer: 180,
+  },
+  // 8월 29일 월요일
+  {
+    task: "Bug groceries",
+    date: "2022-08-29T12:41:21",
+    isDone: true,
+    timer: 30,
+  },
+  {
+    task: "60 mins plank",
+    date: "2022-08-29T12:41:21",
+    isDone: false,
+    timer: 60,
+  },
+  {
+    task: "Take vitamins",
+    date: "2022-08-29T12:41:21",
+    isDone: true,
+    timer: 0,
+  },
+  {
+    task: "Study Typescript",
+    date: "2022-08-29T12:41:21",
+    isDone: true,
+    timer: 200,
+  },
+  {
+    task: "Learn Angular",
+    date: "2022-08-29T12:41:21",
+    isDone: true,
+    timer: 90,
+  },
+  // 8월 30일 화요일
+  {
+    task: "buy milk",
+    date: "2022-08-30T12:41:21",
+    isDone: true,
+    timer: 5,
+  },
+  {
+    task: "buy bananas",
+    date: "2022-08-30T12:41:21",
+    isDone: false,
+    timer: 60,
+  },
+  {
+    task: "tim horton's",
+    date: "2022-08-30T12:41:21",
+    isDone: true,
+    timer: 0,
+  },
+  {
+    task: "Update project plan ",
+    date: "2022-08-30T12:41:21",
+    isDone: false,
+    timer: 172,
+  },
+  {
+    task: "create a meeting agenda",
+    date: "2022-08-29T12:41:21",
+    isDone: true,
+    timer: 0,
+  },
+];
---
+++ src/controllers/todoController.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { client } from "./client";
+import { dummyTodosList } from "./dummy";
+
+// TODO: 백엔드 연결
+
+export const getTodoList = (date, req) => {
+  try {
+    // const response = await client.get("/todo", { params: { date } });
+
+    const isTargetDate = (element) => {
+      const formatedDate = new Date(element.date);
+      return formatedDate.getDate() === date.getDate();
+    };
+    const dummyTodosFromDate = dummyTodosList.filter(isTargetDate);
+
+    return dummyTodosFromDate;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const postTodo = async (todo, req) => {
+  // const response = client.post("/login");
+};
+
+export const updateTodo = async (id, req) => {
+  // const response = client.put("/todo/${id}");
+};
