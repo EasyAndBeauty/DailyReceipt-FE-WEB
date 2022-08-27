3040f947de51398066c108586001dacda6d4c408
jayjeong8 <zuni4242@gmail.com>
2022-08-28 03:12:35 +0900

    refactor : getQuotes를 useEffect로 수정

--- src/components/atoms/receiptQuotes.jsx
+++ src/components/atoms/receiptQuotes.jsx
@@ -1,11 +1,32 @@
 import styled from "styled-components";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export function ReceiptQuotes() {
+  // const getQuotes = async () => {
+  //   try{
+  //     const res = await fetch("https://api.adviceslip.com/advice");
+  //
+  //     if(!res.ok) throw new Error("오늘은 명언이 없어도 괜찮은 하루일지도 몰라요.");
+  //
+  //     const data = await res.json();
+  //     setQuotes(data);
+  //   }catch(e){
+  //     console.error(e);
+  //   }
+  // }
+
   const [quotesState, setQuotes] = useState();
-  fetch("https://api.adviceslip.com/advice")
-    .then((response) => response.json())
-    .then((data) => setQuotes(data.slip.advice));
+
+  useEffect(() => {
+    const getQuotes = async () => {
+      await fetch("https://api.adviceslip.com/advice")
+        .then((response) => response.json())
+        .then((data) => setQuotes(data.slip.advice))
+        .catch((e) => console.error(e));
+    };
+
+    getQuotes();
+  }, []);
 
   return <Quotes>{quotesState}</Quotes>;
 }
