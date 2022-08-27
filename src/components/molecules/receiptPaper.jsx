93e855c2c424da7940f5c989da1808fa12d6d030
jayjeong8 <zuni4242@gmail.com>
2022-08-28 03:13:48 +0900

    refactor : 종이질감 img에 alt 추가

--- src/components/molecules/receiptPaper.jsx
+++ src/components/molecules/receiptPaper.jsx
@@ -5,7 +5,10 @@ export function ReceiptPaper() {
   return (
     <>
       <Container>
-        <Texture src="https://img.freepik.com/free-vector/crumpled-paper-texture_1048-2259.jpg?w=1480&t=st=1661600618~exp=1661601218~hmac=b19bc6461fe0adb3315567718519d901453733476abbf08b177d4f43384b4f07" />
+        <Texture
+          src="https://img.freepik.com/free-vector/crumpled-paper-texture_1048-2259.jpg?w=1480&t=st=1661600618~exp=1661601218~hmac=b19bc6461fe0adb3315567718519d901453733476abbf08b177d4f43384b4f07"
+          alt="paper texture"
+        />
         <TrianglePosition y="6px">
           <ReceiptPaperTriangle />
         </TrianglePosition>
@@ -34,8 +37,6 @@ const Texture = styled.img`
   opacity: 70%;
   mix-blend-mode: multiply;
   z-index: 10;
-  //mask-image: url(#receipt);
-  //-webkit-mask-image: url(#receipt);
 `;
 
 const TrianglePosition = styled.div`
