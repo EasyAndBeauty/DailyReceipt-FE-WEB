9e90d40c8a7192c3f67d0ad7f73c726d5ed0bd16
jayjeong8 <zuni4242@gmail.com>
2022-08-29 04:46:46 +0900

    style : 스타일 수정

--- src/components/atoms/HeaderText/style.jsx
+++ src/components/atoms/HeaderText/style.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Text = styled.h1`
   color: ${(props) => props.theme.wt};
-  font-size: 1.5rem;
+  font-size: 1.75rem;
   //text-align: baseline;
   font-weight: 700;
   //line-height: 200%;
