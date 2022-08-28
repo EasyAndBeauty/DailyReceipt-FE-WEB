b2709041235f21ccd01f7e24b1b27eb583f7439b
Madang <chooi9522@gmail.com>
2022-08-29 03:14:15 +0900

    fix :  URL 항목 오탈자 수정

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,7 +4,7 @@ export const getKakaoLogin = async () => {
   try {
     const response = await client.get(process.env.REACT_APP_KAKAO_BASE_URL, {
       params: {
-        client: process.env.REACT_APP_KAUTH_JS_CLIENT_ID,
+        client_id: process.env.REACT_APP_KAUTH_JS_CLIENT_ID,
         redirect_uri: process.env.REACT_APP_KAUTH_REDIRECT_URI,
         response_type: "code",
       },
@@ -23,3 +23,5 @@ export const getKakaoToken = async (code) => {
 
   return response.data;
 };
+
+export const getUserName = async (user_id) => {};
