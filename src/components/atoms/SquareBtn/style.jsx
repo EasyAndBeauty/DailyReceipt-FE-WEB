419ed57946c069e49edbef8589832666c93e7dd2
jayjeong8 <zuni4242@gmail.com>
2022-08-29 00:15:54 +0900

    style : 스타일 수정

--- src/components/atoms/SquareBtn/style.jsx
+++ src/components/atoms/SquareBtn/style.jsx
@@ -1,15 +1,18 @@
 import styled from "styled-components";
 
 export const BtnContainer = styled.button`
-  width: 16.25rem;
-  height: 3.5rem;
-  border-radius: 0.25rem;
-  background-color: rgba(1, 1, 1, 0.7);
-  color: white;
+  //width: 16.25rem;
+  width: 100%;
+  height: 64px;
+  //border-radius: 0.75rem 0.75rem 0 0;
+  background-color: ${(props) => props.theme.wt};
+  color: ${(props) => props.theme.bk};
+  font-family: "Courier Prime", monospace;
   font-size: 1rem;
   text-align: center;
-  font-weight: 700;
-  line-height: 150%;
+  //font-weight: 700;
+  //line-height: 150%;
+
   &:hover {
     background-color: rgba(1, 1, 1, 0.9);
   }
--- src/components/molecules/TodoHeader/style.jsx
+++ src/components/molecules/TodoHeader/style.jsx
@@ -2,14 +2,23 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100%;
-  margin: 2.75rem 0;
+  //height: 100%;
+  margin: 44px 0 8px;
+  padding: 8px;
   display: flex;
-  flex-direction: row;
-  align-items: flex-start;
+  align-items: center;
   justify-content: space-between;
+  box-sizing: border-box;
+
+  > div {
+    display: flex;
+    align-items: center;
+    margin-left: 8px;
+  }
 `;
 
 export const Btn = styled.button`
   background-color: transparent;
   margin: 0 0.5rem;
+  color: ${(props) => props.wt};
 `;
