decf8226f344c618abcaee242dbe0320b0e6af23
yangsnagwoo <dndb3599@gmail.com>
2022-09-01 14:36:42 +0900

    feat: issue #107 해결

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,4 @@
 export const POMODORO_TIME = 25;
-export const DLAY_TIME = 600; // 60000
+// export const DLAY_TIME = 600; // Test delay time
+export const DLAY_TIME = 60000; // deployment delay time
 export const ELLIPSISLENGTH = 20;
