c386f9d9a035b28d03176b968ba2bc0a31c8933e
Madang <chooi9522@gmail.com>
2022-09-02 21:00:04 +0900

    feature : #114 Todo Get, Delete API 더미화

--- src/controllers/todoController.js
+++ src/controllers/todoController.js
@@ -1,22 +1,31 @@
+import axios from "axios";
 import { client } from "./client";
+import { dummyTodosList } from "./dummy";
 
 // 오늘 날짜 설정
 const today = new Date();
 const FORMATTED_TODAY = today.toISOString().split("T")[0];
 
-export const getTodoList = async (user_id, date = FORMATTED_TODAY, req) => {
+export const getTodoList = async (date = FORMATTED_TODAY) => {
   try {
-    const response = await client.get(`/v2/todo/${user_id}`, {
-      params: { date },
+    // const response = await client.get('/v2/todo', {
+    //   params: { date },
+    // });
+
+    // return response.data;
+
+    const dummyTodosFromDate = dummyTodosList.filter((todo) => {
+      const formatedDate = new Date(todo.date);
+      return formatedDate.getDate() === date.getDate();
     });
 
-    return response.data;
+    return dummyTodosFromDate;
   } catch (err) {
     console.log(err);
   }
 };
 
-export const postTodo = async (user_id, todo, req) => {
+export const postTodo = async (user_id, todo) => {
   try {
     const response = await client.post(`/v2/todo/${user_id}`, todo);
 
@@ -27,13 +36,25 @@ export const postTodo = async (user_id, todo, req) => {
   }
 };
 
-export const updateTodo = async (id, todo, req) => {
+export const updateTodo = async (id, todo) => {
   try {
     const response = await client.put(`/v1/todo/${id}`, todo);
 
     return response.data;
   } catch (error) {
     console.log("Todo를 수정하지 못했습니다.");
+    console.log(error);
+  }
+};
+
+export const deleteTodo = async (todoId) => {
+  try {
+    const response = await axios.get("https://httpstat.us/200");
+    console.log("다음의 Todo 항목을 삭제합니다.");
+    console.log(todoId);
+
+    if (response.ok) return;
+  } catch (error) {
     console.log(error);
   }
 };
