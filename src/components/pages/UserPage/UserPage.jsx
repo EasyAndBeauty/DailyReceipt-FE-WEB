149a14a57e5c860f97cae836c4b22316756c2a7a
Madang <chooi9522@gmail.com>
2022-09-11 15:01:43 +0900

    feature : My화면 닉네임 표시 삭제

--- src/components/pages/UserPage/UserPage.jsx
+++ src/components/pages/UserPage/UserPage.jsx
@@ -1,4 +1,3 @@
-import React, { useState, useContext } from "react";
 import * as S from "./UserPage.styles";
 // import AtuhContext from "store/authContext";
 import { MyHeader } from "components/molecules/MyHeader";
@@ -6,23 +5,11 @@ import { MySection } from "components/organisms/MySection";
 import { MyFooter } from "components/molecules/MyFooter";
 
 export const UserPage = () => {
-  const [userInfo, setUserInfo] = useState("새로운 사용자");
-  // const authCtx = useContext(AtuhContext);
-
-  // 서버와 연결이 되어있지 않음, 아직 사용 불가능
-  // const showUserInfo = async () => {
-  //   const { nickname } = await getUserInfo(authCtx.token);
-  //   setUserInfo(nickname);
-  // };
-
-  // useEffect(() => {
-  //   showUserInfo();
-  // }, []);
   return (
     <S.MyReceiptContainer>
-      <MyHeader userInfo={userInfo} />
+      <MyHeader />
       <MySection />
-      {/* {authCtx.isLoggedIn && <MyFooter />} */}
+      <MyFooter />
     </S.MyReceiptContainer>
   );
 };
