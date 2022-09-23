3582663304d1fa99555b14a2919e0d588ea87b76
Madang <chooi9522@gmail.com>
2022-09-23 12:44:01 +0900

    fix #152 : 객체 반환

--- src/controllers/todoController.js
+++ src/controllers/todoController.js
@@ -5,22 +5,22 @@ export const useTodoClient = () => {
 
   // Todo: 백엔드 갱신 후 API요청 URL 수정
   const getTodoList = async (date) => {
-    return await authClient.get("/v1/todo", {
+    return await authClient.get("/api/v1/todo", {
       params: { date },
     });
   };
 
   const postTodo = async (todo) => {
-    return await authClient.post(`/v1/todo`, todo);
+    return await authClient.post(`/api/v1/todo`, todo);
   };
 
   const updateTodo = async (todoId, todo) => {
-    return await authClient.put(`/v1/todo/${todoId}`, todo);
+    return await authClient.put(`/api/v1/todo/${todoId}`, todo);
   };
 
   const deleteTodo = async (todoId) => {
-    return await authClient.delete(`/v1/todo/${todoId}`);
+    return await authClient.delete(`/api/v1/todo/${todoId}`);
   };
 
-  return [getTodoList, postTodo, updateTodo, deleteTodo];
+  return { getTodoList, postTodo, updateTodo, deleteTodo };
 };
