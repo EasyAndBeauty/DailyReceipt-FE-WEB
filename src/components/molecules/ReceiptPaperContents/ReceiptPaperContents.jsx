796f1c2bc5772db2c9dbeccec6c723dd178695a8
gygy7151 <gygy7151@gmail.com>
2022-10-09 23:42:50 +0900

    Feature/BlueSetDefaultForTimer

--- src/components/molecules/ReceiptPaperContents/ReceiptPaperContents.jsx
+++ src/components/molecules/ReceiptPaperContents/ReceiptPaperContents.jsx
@@ -1,8 +1,8 @@
 import {
-  ReceiptTodo,
-  ReceiptPaperInfo,
-  ReceiptBarcode,
-  ReceiptQuotes,
+	ReceiptTodo,
+	ReceiptPaperInfo,
+	ReceiptBarcode,
+	ReceiptQuotes,
 } from "components";
 import * as S from "./ReceiptPaperContents.styles";
 
@@ -15,38 +15,38 @@ import * as S from "./ReceiptPaperContents.styles";
  * @returns
  */
 export function ReceiptPaperContents({ todos }) {
-  const line = "-----------------------------------";
-  let timerTotal = 0;
-  return (
-    <S.Paper>
-      <S.Title>RECEIPT</S.Title>
-      <ReceiptPaperInfo />
-      <div>{line}</div>
-      <S.TodoContainer>
-        {todos.map((todo, index) => {
-          timerTotal += todo.timer;
-          return <ReceiptTodo key={todo.todoId + index}>{todo}</ReceiptTodo>;
-        })}
-      </S.TodoContainer>
-      <div>{line}</div>
-      <S.TotalContainer>
-        <div>
-          <span>ITEM COUNT :</span>
-          <span>{todos.length}</span>
-        </div>
-        <div>
-          <span>TOTAL :</span>
-          <span>
-            {Math.floor(timerTotal / 60)}:
-            {(timerTotal % 60).toString().padStart(2, "0")}
-          </span>
-        </div>
-      </S.TotalContainer>
-      <div>{line}</div>
-      <ReceiptQuotes />
-      <div>{line}</div>
-      <ReceiptBarcode />
-      <div>{line}</div>
-    </S.Paper>
-  );
+	const line = "-----------------------------------";
+	let timerTotal = 0;
+	return (
+		<S.Paper>
+			<S.Title>RECEIPT</S.Title>
+			<ReceiptPaperInfo />
+			<div>{line}</div>
+			<S.TodoContainer>
+				{todos.map((todo, index) => {
+					timerTotal += todo.timer;
+					return <ReceiptTodo key={todo.todoId + index}>{todo}</ReceiptTodo>;
+				})}
+			</S.TodoContainer>
+			<div>{line}</div>
+			<S.TotalContainer>
+				<div>
+					<span>ITEM COUNT :</span>
+					<span>{todos.length}</span>
+				</div>
+				<div>
+					<span>TOTAL :</span>
+					<span>
+						{Math.floor(timerTotal / 60)}:
+						{(timerTotal % 60).toString().padStart(2, "0")}
+					</span>
+				</div>
+			</S.TotalContainer>
+			<div>{line}</div>
+			<ReceiptQuotes />
+			<div>{line}</div>
+			<ReceiptBarcode />
+			<div>{line}</div>
+		</S.Paper>
+	);
 }
