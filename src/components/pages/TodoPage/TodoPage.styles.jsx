f231bf6d279b884a9b650d10c3cec191ddf8e0f0
jayjeong8 <zuni4242@gmail.com>
2022-10-30 21:15:28 +0900

    style: 영수증 출력 버튼의 그림자 수정

--- src/components/pages/TodoPage/TodoPage.styles.jsx
+++ src/components/pages/TodoPage/TodoPage.styles.jsx
@@ -46,6 +46,6 @@ export const Bottom = styled.div`
     display: flex;
     position: relative;
     top: 6px;
-    box-shadow: 0px -10px 5px 0px rgba(0, 0, 0, 0.2);
+    box-shadow: 5px -20px 15px 2px rgba(10, 10, 10, 0.2);
   }
 `;
