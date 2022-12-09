85fb00e480103573479645b5a497adfd30c66f96
Madang <chooi9522@gmail.com>
2022-12-10 03:18:13 +0900

    refactor #205 : 오타수정

--- src/components/atoms/TextBtn/TextBtn.jsx
+++ src/components/atoms/TextBtn/TextBtn.jsx
@@ -5,7 +5,7 @@ import * as S from "./TextBtn.styles";
  *
  * 텍스트가 있는 버튼입니다.
  *
- * @param {Function} onClick1 - 버튼을 클릭했을 때 실행되는 함수
+ * @param {Function} onClick - 버튼을 클릭했을 때 실행되는 함수
  * @param {String} text - 버튼에 표시될 텍스트
  * @param {String} color - 텍스트의 색 ※context에 지정된 색만 사용할 것 "bk"(default) / "wt" / "red" / "gray" / "green" / "lightGray"
  * @param {String} type - 텍스트의 타입  "default" / "bold"
