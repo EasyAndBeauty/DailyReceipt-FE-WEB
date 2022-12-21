ad3108f1438ba552c1cd43659050ee810473828a
Madang <chooi9522@gmail.com>
2022-12-21 23:00:02 +0900

    feature #195 : 핀버튼 기능 완성

--- src/components/pages/ReceiptPage/ReceiptPage.jsx
+++ src/components/pages/ReceiptPage/ReceiptPage.jsx
@@ -39,7 +39,7 @@ export function ReceiptPage() {
       <S.IconContainer>
         <CopyBtn />
         <SaveBtn date={date} />
-        <PinBtn isPinned={pinned} toggleVisible={(bool) => setPinBtnModalVisible(bool)} />
+        <PinBtn isPinned={pinned} openModal={() => setPinBtnModalVisible(true)} />
       </S.IconContainer>
       {modalOn && <AlertModal onClick={() => setModalOn(false)} />}
       {pinBtnModalVisible && <PinnedModal onClose={() => setPinBtnModalVisible(false)} />}
